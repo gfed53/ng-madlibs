ad84c3007fd3105e518c6740405b6b940b992a30
gfed53 <gfederico7153@gmail.com>
2016-01-29 13:02:55 -0500

    Added controller, gender selection in HTML

---
+++ myApp.js
@@ -0,0 +1,19 @@
+angular.module('myApp', [])
+.controller('myCtrl', function($scope){
+	$scope.being = "";
+	$scope.name = "";
+	$scope.heOrShe = "";
+	$scope.adjective = "";
+	$scope.verb = "";
+	$scope.actionVerb = "";
+	$scope.event = "";
+	$scope.noun = "";
+	$scope.adverb = "";
+	$scope.adjective2 = "";
+	$scope.pluralSnackFood = "";
+	$scope.celebrity = "";
+	$scope.number = "";
+	$scope.himOrHer = "";
+	$scope.pastTenseVerb = "";
+	$scope.mood = "";
+});
