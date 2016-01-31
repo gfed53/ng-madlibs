6be38f3b7d58fafaad49387ee3cb892adc0c832d
gfed53 <gfederico7153@gmail.com>
2016-01-31 14:03:30 -0500

    Words section properly hides on validation, not solely on 'submit'

--- myApp.js
+++ myApp.js
@@ -17,18 +17,18 @@ angular.module('myApp', ['ngMessages'])
 	$scope.number = "";
 	$scope.adjective3 = "";
 	$scope.mood = "";
-	$scope.submitted = false;
+	$scope.validUponSub = false;
 	$scope.submit = function(){
 		if($scope.myForm.$valid){
 			console.log("Should be good");
-			$scope.submitted = true;
+			$scope.validUponSub = true;
 		} else {
 			console.log("There's an error");
 		}
 	};
 	$scope.startOver = function(){
 		$scope.being = $scope.name = $scope.gender.name = $scope.adjective = $scope.adjective = $scope.verb = $scope.actionVerb = $scope.event = $scope.noun = $scope.adverb = $scope.adjective2 = $scope.pluralSnackFood = $scope.celebrity = $scope.number = $scope.adjective3 = $scope.mood = "";
-		$scope.submitted = false;
+		$scope.validUponSub = false;
 		console.log("Starting over");
 	};
 });
